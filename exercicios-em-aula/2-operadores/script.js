09fafa32a889e9b328020314b62c747aea6668af
Patricia Magalhães <paath.magalhaes2@gmail.com>
2022-04-30 19:56:21 -0300

    Inserindo resolução exercícios em aula

--- exercicios-em-aula/2-operadores/script.js
+++ exercicios-em-aula/2-operadores/script.js
@@ -13,21 +13,32 @@
 
 
 // 1. Receba dois números por meio do prompt
+let x = Number(prompt("Digite um número: "))
+let y = Number(prompt("Digite outro número: "))
 
 
 // 2. Informe na tela a soma desses números
+alert((`O resultado da soma entre ${x} e ${y} é ${x+y}`))
 
 
 // 3. Informe o resultado da subtração desses números
+alert((`O resultado da subtração entre ${x} e ${y} é ${x-y}`))
 
 
 // 4. Informe o produto desses números
+alert((`O resultado da multiplicação entre ${x} e ${y} é ${x*y}`))
 
 
 // 5. Receba dois números inteiros por meio do prompt
+let n1 = Number(prompt("Digite um número: "))
+let n2 = Number(prompt("Digite outro número: "))
+
 
 
 // 6. Informe o resultado da divisão do primeiro pelo segundo
+alert((`O resultado da divisão entre ${n1} por ${n2} é ${n1/n2}`))
+
 
 
 // 7. Informe o resto da divisão do primeiro pelo segundo
+alert((`O resto da divisão entre ${n1} por ${n2} é ${n1%n2}`))
