cbe939af1cf8d41013f7900dcd1d46b870158f4a
Mariana Pequeno <pequeno.mariana@gmail.com>
2022-04-30 12:58:28 -0300

    Finalização do exercicio 1-variaveis

--- exercicios-em-aula/1-variaveis/script.js
+++ exercicios-em-aula/1-variaveis/script.js
@@ -4,27 +4,32 @@
     https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Grammar_and_types
  */
 
-
 // 1. Crie uma variável que armazene a mensagem "Olá, Mundo!" e a imprima no console
-
+console.log(`Olá, mundo!`);
 
 // 2. Crie uma variável que armazene seu nome
-
+const nome = "Mariana";
 
 // 3. Crie uma variável que armazene o ano de seu nascimento
-
+const anoNascimento = 1990;
 
 // 4. Crie uma variável que indique se hoje foi seu primeiro contato com javascript
-
+const primeiroContatoJavascript = false;
 
 // 5. Imprima, no console, seu nome, o ano de seu nascimento
-//    e se já teve contato com javascript (usando as variáveis criadas acima!)
-
+//e se já teve contato com javascript (usando as variáveis criadas acima!)
+console.log(nome, anoNascimento, primeiroContatoJavascript);
 
 // 6. Usando a função typeof, imprima no console o tipo das variáveis criadas
-
+console.log(typeof nome);
+console.log(typeof anoNascimento);
+console.log(typeof primeiroContatoJavascript);
 
 // 7. Crie uma variável sem nenhum valor atribuído e imprima seu valor e seu tipo no console
-
+let variavel; //undefined
+console.log(variavel);
 
 // 8. Crie uma variável inicializada com o valor nulo e imprima seu valor e seu tipo no console
+let nula = null;
+console.log(nula);
+console.log(typeof nula);
