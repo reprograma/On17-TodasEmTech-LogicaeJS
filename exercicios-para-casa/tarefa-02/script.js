dea788b3a2511d6ba6e1b6c9c82479ae2600a3c3
Patricia Magalhães <paath.magalhaes2@gmail.com>
2022-04-30 19:56:30 -0300

    Inserindo resolução exercícios para casa

--- exercicios-para-casa/tarefa-02/script.js
+++ exercicios-para-casa/tarefa-02/script.js
@@ -6,3 +6,8 @@
     3. Informe ao usuário o valor da mercadoria após o desconto;
     4. Informe ao usuário qual quantia foi economizada por conta do desconto.
  */
+
+let preco = parseFloat(prompt("Informe o preço da mercadoria: "));
+let desconto = parseFloat(prompt("Informe o percentual de desconto: "))/100;
+let valorAPagar = preco - (preco * desconto);
+let valorDesconto = preco - valorAPagar;
--- exercicios-para-casa/tarefa-06/script.js
+++ exercicios-para-casa/tarefa-06/script.js
@@ -5,3 +5,9 @@
     2. Informe o total da conta do cliente;
     3. Exiba uma mensagem com o valor do troco.
  */
+
+let pagamento = parseFloat(prompt("Quantia dada pelo cliente: "));
+let total = parseFloat(prompt("Total da compra: "));
+let troco = pagamento - total;
+console.log(`Valor do troco: R$${troco}`)
+
