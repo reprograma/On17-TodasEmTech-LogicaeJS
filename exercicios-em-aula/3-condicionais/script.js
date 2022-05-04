bb8e4a9defdcc67f3a657aed0af1751f76233822
Mayara Rocha <mayaraaraujorocha54@gmail.com>
2022-05-04 00:43:37 -0300

    Exercicios em aula Condicionais concluido

--- exercicios-em-aula/3-condicionais/script.js
+++ exercicios-em-aula/3-condicionais/script.js
@@ -23,6 +23,25 @@
 */
 
 let letraEscolhida = prompt("insira uma letra qualquer do alfabeto").toLowerCase();
+ if (letraEscolhida === 'a'||letraEscolhida === 'e'
+ || letraEscolhida === 'i' || letraEscolhida === 'o'
+ || letraEscolhida === 'u'){
+   console.log('Essa letra é uma vogal');
+   } else {
+     console.log('Essa letra não é uma vogal');
+   }
+  
+   switch (letraEscolhida) {
+     case 'a':
+     case 'e':
+     case 'i':
+     case 'o':
+     case 'u':
+       alert('É uma vogal')
+       break;
+       default: 
+       alert('Não é uma vogal')
+   }
 
 // 
 
@@ -32,10 +51,43 @@ let letraEscolhida = prompt("insira uma letra qualquer do alfabeto").toLowerCase
 
 let numeroEscolhido = prompt("Insira um numero de 1-7");
 
+switch (numeroEscolhido){
+  case '1':
+    console.log('segunda');
+    break;
+  case '2':
+    console.log('terça');
+    break;  
+  case '3':
+      console.log('quarta'); 
+      break; 
+  case '4':
+    console.log('quinta');
+    break;
+  case '5':
+    console.log('sexta');
+    break; 
+  case '6':
+    console.log('sabado');
+    break; 
+  case '7':
+    console.log('domingo')
+    break; 
+}
+
 /* 
   3. Elabore um algoritmo que receba dois números e determine qual é o maior entre eles, se ambos os números forem iguais, mostre uma mensagem no console "Os números são iguais";
 */
+const num1 = Number (prompt("Insira um número"));
+const num2 = Number(prompt("Insira outro número"));
 
+if (num1 > num2) {
+  alert(num1);
+  } else if (num2 > num1) {
+    alert(num2);
+  } else {
+    console.log("Os números são iguais")
+  }
 
 /*
   4. Crie um algoritmo que receba três notas de um aluno, calcule sua média
@@ -49,14 +101,16 @@ let numeroEscolhido = prompt("Insira um numero de 1-7");
     Alunos de recuperação devem ver a mensagem: Você está de recuperação!
     Alunos reprovados devem ver a mensagem: Que pena, você foi reprovado.
 */
+ const nota1 = Number (prompt("Insira a primeira Nota"));
+ const nota2 = Number (prompt("Insira a segunda Nota"));
+ const nota3 = Number (prompt("Insira a terceira Nota"));
 
-/*
- 5. Crie um algoritmo que receba dois números e some todos os números
-    começados no primeiro e terminados no segundo
-
-    Faça duas versões deste exercício: usando for e usando while
-
-    Exemplo:
-        se os números informados forem 2 e 7, seu programa deve retornar 27,
-        pois 2 + 3 + 4 + 5 + 6 + 7 = 27
-*/
+ let media = (nota1 + nota2 + nota3) / 3;
+ 
+ if (media >= 7){
+   alert("Parabéns, você foi aprovado, aproveite suas férias!");
+ } else if (media >= 5 && media < 7){
+   alert(" Você está de recuperação!");
+  } else {
+    alert("Que pena, você foi reprovado.")
+  }
