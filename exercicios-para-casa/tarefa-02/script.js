b1aa22f0d05b5e0869e475ff912b5e861e968626
iiizadora <oliveiras.izadora@gmail.com>
2022-05-04 20:57:43 -0300

    Exercicio para casa tarefa-02 finalizado

--- exercicios-para-casa/tarefa-02/script.js
+++ exercicios-para-casa/tarefa-02/script.js
@@ -6,3 +6,12 @@
     3. Informe ao usuário o valor da mercadoria após o desconto;
     4. Informe ao usuário qual quantia foi economizada por conta do desconto.
  */
+
+let price = Number(
+  prompt("Informe o preço da mercadoria(apenas números): R$ "),
+);
+let percentual = Number(prompt("Informe o valor de desconto:"))/100;
+let discount = price - price * percentual;
+let valueDiscount = price - discount;
+alert(`O preço da mercadoria após o desconto é: R$ ${discount}`);
+alert(`O valor economizado foi de R$ ${valueDiscount}`)
