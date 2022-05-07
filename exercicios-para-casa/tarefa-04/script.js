a840e586b3c6e682229c95c38a6b6410a57bb8a1
Emanueli Martins <emanueli.martiins@gmail.com>
2022-05-07 19:15:44 -0300

    resolução tarefa 4-5-6

--- exercicios-para-casa/tarefa-04/script.js
+++ exercicios-para-casa/tarefa-04/script.js
@@ -4,3 +4,12 @@
     1. Receba o preço de cada um dos 10 itens;
     2. Mostre na tela a soma de todos os preços.
  */
+
+    let precoItens = 0;
+
+    for (let i = 1; i <= 10; i++) {
+      precoItens += parseFloat(prompt(`Digite o valor do item ${i}`));
+    }
+    
+    alert (`O total dos itens é de R$${valorItem.toFixed(2)}`);
+     
