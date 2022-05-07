5a2654ba5ac380598fa542bd4c6ad34ec567205a
andressa05gomes <andressa05gomes@gmail.com>
2022-05-07 17:56:01 -0300

    Licao-Andressa

--- exercicios-em-aula/1-variaveis/script.js
+++ exercicios-em-aula/1-variaveis/script.js
@@ -6,23 +6,29 @@
 
 
 // 1. Crie uma variável que armazene a mensagem "Olá, Mundo!" e a imprima no console
-
+let mensagem = "Olá, mundo!";
+console.log(mensagem);
 
 // 2. Crie uma variável que armazene seu nome
-
+let myName
+myName = "Andressa";
+console.log (myName)
 
 // 3. Crie uma variável que armazene o ano de seu nascimento
-
+let aNumber = 1997;
+console.log (aNumber)
 
 // 4. Crie uma variável que indique se hoje foi seu primeiro contato com javascript
-
+let aText = "Este é meu primeiro contato com javascript!"
+console.log (aText)
 
 // 5. Imprima, no console, seu nome, o ano de seu nascimento
 //    e se já teve contato com javascript (usando as variáveis criadas acima!)
-
+let phrase1 = "Olá, me chamo" + myName + "nasci" + aNumber + aText;
+console.log ("Olá, me chamo", myName, "nasci em", aNumber, aText)
 
 // 6. Usando a função typeof, imprima no console o tipo das variáveis criadas
-
+ console.log((typeof)nome)
 
 // 7. Crie uma variável sem nenhum valor atribuído e imprima seu valor e seu tipo no console
 
