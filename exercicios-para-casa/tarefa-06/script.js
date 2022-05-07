14d7ffbe224f2638befd13ab7d9580053be88475
Nicolle-Dias <nicolledias1992@gmail.com>
2022-05-07 18:22:39 -0300

    Atividade resolvida.

--- exercicios-para-casa/tarefa-06/script.js
+++ exercicios-para-casa/tarefa-06/script.js
@@ -5,3 +5,8 @@
     2. Informe o total da conta do cliente;
     3. Exiba uma mensagem com o valor do troco.
  */
+
+let clientTotal = prompt("Informe o total do cliente: ");
+let clientPayment = prompt("Informe o quanto o cliente pagou: ");
+let change = clientTotal - clientPayment;
+alert("O troco é: " + change + ".")
