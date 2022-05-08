617ef89b7641efc94e9e73ff848860d5b30909f0
iiizadora <oliveiras.izadora@gmail.com>
2022-05-07 23:15:42 -0300

    Exercicio para casa tarefa-06

--- exercicios-para-casa/tarefa-06/script.js
+++ exercicios-para-casa/tarefa-06/script.js
@@ -5,3 +5,9 @@
     2. Informe o total da conta do cliente;
     3. Exiba uma mensagem com o valor do troco.
  */
+
+    let moneyClient = parseFloat(prompt('Qual valor recebido do cliente?'));
+    let countClient = parseFloat(prompt('Qual o valor da conta do cliente?'));
+    let changeClient = moneyClient-countClient;
+
+    alert(`O valor do troco é R$${changeClient},00`)
