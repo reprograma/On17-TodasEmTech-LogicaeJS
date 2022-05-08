6740caeb5b626c489b18fb5f52ebd1b511c053a1
iiizadora <oliveiras.izadora@gmail.com>
2022-05-07 22:57:44 -0300

    Exercicio para casa tarefa-04 finalizado

--- exercicios-para-casa/tarefa-04/script.js
+++ exercicios-para-casa/tarefa-04/script.js
@@ -4,3 +4,9 @@
     1. Receba o preço de cada um dos 10 itens;
     2. Mostre na tela a soma de todos os preços.
  */
+
+let price = 0;
+for (let i = 1; i <= 10; i++) {
+  price += parseFloat(prompt("Digite o preço do item: R$"));
+}
+alert(`A soma dos produtos é: ${price}`);
