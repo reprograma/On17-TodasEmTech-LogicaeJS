9a8ec0cfc06192bbb120525c79c46b46077457fb
iiizadora <oliveiras.izadora@gmail.com>
2022-05-07 23:05:43 -0300

    Exercicio para casa tarefa-05 finalizado

--- exercicios-para-casa/tarefa-05/script.js
+++ exercicios-para-casa/tarefa-05/script.js
@@ -5,3 +5,12 @@
     2. Receba o valor de cada um dos itens da compra;
     3. Informe o total gasto na compra.
  */
+
+ let quantidadeItens = Number(prompt('Digite o quantidade de produtos:'))  
+ let totalGasto = 0
+ 
+ for(let i =1; i<= quantidadeItens;i++){
+     let valorCompra = parseFloat(prompt('Digite o valor do produto'))
+     totalGasto += valorCompra
+ }
+alert(`O valor total gasto nas compras foi de R$${totalGasto},00`)
