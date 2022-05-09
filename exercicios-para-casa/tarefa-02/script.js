b0939f5a55f5f392409494e729c9cdd246e5253a
Lomaiaa <lorena14ma@yahoo.com.br>
2022-05-09 10:34:49 -0300

    entrega-lorena-araujo

--- exercicios-para-casa/tarefa-02/script.js
+++ exercicios-para-casa/tarefa-02/script.js
@@ -6,3 +6,11 @@
     3. Informe ao usuário o valor da mercadoria após o desconto;
     4. Informe ao usuário qual quantia foi economizada por conta do desconto.
  */
+
+
+let precoMercadoria = parseFloat(prompt("Digite o preço da mercadoria:"));
+let percentualDesconto = parseFloat(prompt("Digite o percentual de desconto:"))/100
+let valorMercadoria = precoMercadoria - (precoMercadoria * percentualDesconto);
+let valorEconomia = precoMercadoria - percentualDesconto;
+
+alert("O valor final de sua mercadoria é de R$ " + valorMercadoria + " e você economizou R$ " + valorEconomia);
--- exercicios-para-casa/tarefa-04/script.js
+++ exercicios-para-casa/tarefa-04/script.js
@@ -4,3 +4,13 @@
     1. Receba o preço de cada um dos 10 itens;
     2. Mostre na tela a soma de todos os preços.
  */
+
+let itens = parseInt(prompt("Qual a quantidade de itens na compra?"));
+
+let i = 1;
+let precoTotal = 0;
+for (let i = 1; i <= itens; i++) {
+    let preco = parseFloat(prompt(`Qual o valor do item ${i}?`))
+    precoTotal += preco
+}
+    alert(`O preço total de todos os itens é de R$${precoTotal}`)
