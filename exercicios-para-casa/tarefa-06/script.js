9798f2a0f850f66ba05c0428aff0745703f20172
Mariana Caetano Pereira <nanah.caetano@gmail.com>
2022-05-08 22:24:41 -0300

    exercício 6

--- exercicios-para-casa/tarefa-06/script.js
+++ exercicios-para-casa/tarefa-06/script.js
@@ -5,3 +5,9 @@
     2. Informe o total da conta do cliente;
     3. Exiba uma mensagem com o valor do troco.
  */
+
+    let cash = Number(prompt("Informe a quantia"));
+    let totalCash = Number(prompt("O total da sua conta é de "));
+
+    let change = (cash - totalCash);
+    alert("O seu troco é de " + change)
