bc165222a32afb4f62dfbf911fb2ed352d33d240
Mariana Caetano Pereira <nanah.caetano@gmail.com>
2022-05-08 22:12:38 -0300

    exercício 4

--- exercicios-para-casa/tarefa-04/script.js
+++ exercicios-para-casa/tarefa-04/script.js
@@ -4,3 +4,14 @@
     1. Receba o preço de cada um dos 10 itens;
     2. Mostre na tela a soma de todos os preços.
  */
+
+    let finalValue = 0;
+
+    for(let i= 1; i <= 10; i++) {
+        console.log("o valor é: " + i);
+        let value = prompt("Valor do item" + i);
+        totalValue += value;
+        console.log(totalValue);
+    }
+
+    alert("O valor total dos itens é de " + totalValue)
