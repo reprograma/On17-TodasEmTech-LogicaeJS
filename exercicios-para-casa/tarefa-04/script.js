29c51591082324a2b13c898ec803f71756a06b39
Larissa Santana <larissa.soliveira89@gmail.com>
2022-05-09 22:24:18 -0300

    finalizacao

--- exercicios-para-casa/tarefa-04/script.js
+++ exercicios-para-casa/tarefa-04/script.js
@@ -4,3 +4,11 @@
     1. Receba o preço de cada um dos 10 itens;
     2. Mostre na tela a soma de todos os preços.
  */
+
+let totalPrecos = 0;
+
+for (let i = 1; i <= 10; i++) {
+  totalPrecos += parseFloat(prompt(`Preço do produto ${i}:`));
+}
+
+alert(`Total da compra: ${totalPrecos.toFixed(2)}`);
--- exercicios-para-casa/tarefa-06/script.js
+++ exercicios-para-casa/tarefa-06/script.js
@@ -5,3 +5,13 @@
     2. Informe o total da conta do cliente;
     3. Exiba uma mensagem com o valor do troco.
  */
+
+let dinheiroCliente = parseFloat(prompt("Quantia dada pelo cliente:"));
+let totalConta = parseFloat(prompt("Total da compra:"));
+let troco = dinheiroCliente - totalConta;
+
+alert(`
+Dinheiro dado pelo cliente: ${dinheiroCliente.toFixed(2)}
+Total da conta: ${totalConta.toFixed(2)}
+Valor do troco: ${troco.toFixed(2)}
+`);
