52341fe7fff1941f53a6b14f5f85d7c28db0a350
Juliana Melo <juliana.melos@outlook.com>
2022-05-13 22:47:07 -0300

    atividades para casa finalizadas

--- exercicios-para-casa/tarefa-03/script.js
+++ exercicios-para-casa/tarefa-03/script.js
@@ -6,3 +6,10 @@
       - caso seja, informe na tela que ela pode comprar bebidas;
       - caso contrário, informe que você não está autorizada a vender álcool para menores.
  */
+      const yearBirth = Number(prompt("diga seu ano de nascimento"));
+
+      if (yearBirth <= 2004){
+        alert("pode comprar bebidas");
+      } else {   
+        alert("proibido a venda para menores de 18 anos")
+      }
