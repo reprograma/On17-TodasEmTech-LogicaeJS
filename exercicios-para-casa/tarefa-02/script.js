c4699adc90befeb0763a629c5614363cb57a213d
Nana Miranda <falecomnanamiranda@gmail.com>
2022-06-28 19:49:48 -0300

    entregando exercícios para casa

--- exercicios-para-casa/tarefa-02/script.js
+++ exercicios-para-casa/tarefa-02/script.js
@@ -1,8 +1,22 @@
-/**
-    Tarefa 02
+//   Tarefa 02
 
-    1. Solicite o preço de uma mercadoria;
-    2. Solicite um percentual de desconto;
-    3. Informe ao usuário o valor da mercadoria após o desconto;
-    4. Informe ao usuário qual quantia foi economizada por conta do desconto.
- */
+    // 1. Solicite o preço de uma mercadoria;
+
+        let precoMercadoria = parseFloat(prompt("Digite o valor da mercadoria"));
+
+    // 2. Solicite um percentual de desconto;
+        
+        let percentualDesconto = parseFloat(prompt("Insira o percentual de desconto:"));
+
+
+    // 3. Informe ao usuário o valor da mercadoria após o desconto;
+
+        let desconto = precoMercadoria - (precoMercadoria * percentualDesconto) / 100;
+        let mercadoriaComDesconto = precoMercadoria - desconto;
+        alert('O valor da mercadoria com desconto é de ${mercadoriaComDesconto}');
+
+    // 4. Informe ao usuário qual quantia foi economizada por conta do desconto.
+
+        let valorEconomizado = precoMercadoria - mercadoriaComDesconto;
+        alert('Você economizou R$ ${valorEconomizado}');     
+ 
--- exercicios-para-casa/tarefa-05/script.js
+++ exercicios-para-casa/tarefa-05/script.js
@@ -5,3 +5,13 @@
     2. Receba o valor de cada um dos itens da compra;
     3. Informe o total gasto na compra.
  */
+
+
+    let quantItens = Number(prompt("Informe a quantidade de itens"));
+    let valorTotal = 0;
+    for (let i = 1; i <= quantItens; i++) {
+        let preco = parseFloat(prompt(`Insira o valor do produto${i}: `))
+        valorTotal += preco
+    }
+ 
+    alert (`O valor total dos itens é de R$${valorTotal}`);
