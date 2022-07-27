15726d3321fcc5b4540a0f8a4752cbe7778b6d40
Luisa Calixto <luisa.calixto@hotmail.com>
2022-07-27 12:57:39 -0300

    Subindo exercício...

--- exercicios-para-casa/tarefa-03/script.js
+++ exercicios-para-casa/tarefa-03/script.js
@@ -1,8 +1,17 @@
-/**
-    Tarefa 03
 
-    1. Solicite o ano de nascimento;
-    2. Verifique se a pessoa é maior de idade:
-      - caso seja, informe na tela que ela pode comprar bebidas;
-      - caso contrário, informe que você não está autorizada a vender álcool para menores.
- */
+    // Tarefa 03
+
+    // 1. Solicite o ano de nascimento;
+
+    let anoNascimento = Number(prompt("Informe seu ano de nascimento:"));
+    let idade = 2022 - anoNascimento;
+
+// 2. Verifique se a pessoa é maior de idade:
+//   - caso seja, informe na tela que ela pode comprar bebidas;
+//   - caso contrário, informe que você não está autorizada a vender álcool para menores.
+     
+  if (idade >= 18) {
+    alert ("Você está autorizado a comprar bebidas alcoólicas.");
+  } else {
+    alert ("Você NÃO está autorizado a comprar bebidas alcoólicas, volte quando tiver 18 anos.");
+  }
